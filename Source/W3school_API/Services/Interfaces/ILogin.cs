7a2e23bd47f5af55edf12e04d3f45cb8521861c3
Sự <sucucvipcucpro@gmail.com>
2021-11-02 17:14:42 +0700

    source code
    full source ever

---
+++ Source/W3school_API/Services/Interfaces/ILogin.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace w3schools_API.Services.Interfaces
+{
+    interface ILogin
+    {
+    }
+}
